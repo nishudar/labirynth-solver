d404ad02d1705947fec1f462c31fce5ea91c20af
Nishudar <12666332+nishudar@users.noreply.github.com>
2023-08-18 22:02:46 +0200

    Better UI

--- Labyrinth.Visualisation/MainWindow.xaml.cs
+++ Labyrinth.Visualisation/MainWindow.xaml.cs
@@ -17,55 +17,54 @@ namespace Visualisation
         public MainWindow() 
             => InitializeComponent();
 
-        private void OnDrawClick(object sender, RoutedEventArgs e)
+        private void DrawMaze(object sender, RoutedEventArgs e)
         {
-            Canv.Children.Clear();
-            Labyrinth labyrinth;
             try
             {
-                labyrinth = LabirynthExtensions.LoadFromText(labirynthText.Text, new AStar(), new DoorKeyTeleportSolver());
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(this, ex.Message, "Cannot resole maze");
-                return;
-            }
+                MazeCanvas.Children.Clear();
+                var labyrinth =
+                    LabirynthExtensions.LoadFromText(MazeText.Text, new AStar(), new DoorKeyTeleportSolver());
 
-            var fields = labyrinth.AllFields().ToArray();
-            var size = (int) Math.Min(Canv.Width/labyrinth.Width, Canv.Height/labyrinth.Height);
+                var fields = labyrinth.AllFields().ToArray();
+                var size = (int) Math.Min(MazeCanvas.Width / labyrinth.Width, MazeCanvas.Height / labyrinth.Height);
 
-            foreach (var field in fields)
-            {
-                var fill = GetFill(field.FieldType, labyrinth.IsPath(field));
-                var rect = new Rectangle
+                foreach (var field in fields)
                 {
+                    var fill = GetFill(field.FieldType, labyrinth.IsPath(field));
+                    var rect = new Rectangle
+                    {
 
-                    Width = size,
-                    Height = size,
-                    Fill = fill,
-                    RadiusX = (double)size/2,
-                    RadiusY = (double)size/3
-                };
-                Canvas.SetLeft(rect, field.X * size);
-                Canvas.SetTop(rect, field.Y * size);
+                        Width = size,
+                        Height = size,
+                        Fill = fill,
+                        RadiusX = (double) size / 2,
+                        RadiusY = (double) size / 3
+                    };
+                    Canvas.SetLeft(rect, field.X * size);
+                    Canvas.SetTop(rect, field.Y * size);
 
-                Canv.Children.Add(rect);
+                    MazeCanvas.Children.Add(rect);
 
-                if (field.FieldType is FieldType.Empty or FieldType.Wall) 
-                    continue;
-                
-                var text = new TextBlock
-                {
-                    Text = field.FieldType.ToFieldChar().ToString(),
-                    Foreground = System.Windows.Media.Brushes.Black,
-                    FontSize = 10,
-                    Margin = new Thickness(0),
-                    Padding = new Thickness(0)
-                };
+                    if (field.FieldType is FieldType.Empty or FieldType.Wall)
+                        continue;
 
-                Canvas.SetLeft(text, Canvas.GetLeft(rect) + 5);
-                Canvas.SetTop(text, Canvas.GetTop(rect));
-                Canv.Children.Add(text);
+                    var text = new TextBlock
+                    {
+                        Text = field.FieldType.ToFieldChar().ToString(),
+                        Foreground = System.Windows.Media.Brushes.Black,
+                        FontSize = 10,
+                        Margin = new Thickness(0),
+                        Padding = new Thickness(0)
+                    };
+
+                    Canvas.SetLeft(text, Canvas.GetLeft(rect) + 5);
+                    Canvas.SetTop(text, Canvas.GetTop(rect));
+                    MazeCanvas.Children.Add(text);
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
             }
         }
 
